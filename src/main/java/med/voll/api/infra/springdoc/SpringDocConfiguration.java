02b8bfaf1cc26cb98b3c74a14cedbf61f2e9a9e7
Fernando Guedes <fernando.guedes@cwi.com.br>
2023-08-28 22:10:18 -0300

    feat: adicionando informações ao Swagger

--- src/main/java/med/voll/api/infra/springdoc/SpringDocConfiguration.java
+++ src/main/java/med/voll/api/infra/springdoc/SpringDocConfiguration.java
@@ -2,6 +2,9 @@ package med.voll.api.infra.springdoc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +14,19 @@ public class SpringDocConfiguration {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        // Configuração para expor o token na documentação
         var key = "bearer-key";
         var securitySchemesItem = new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT");
         var components = new Components().addSecuritySchemes(key, securitySchemesItem);
-        return new OpenAPI().components(components);
+
+        // Configuração de informações na documentação
+        var tituloApi = "Voll.med API";
+        var descricaoApi = "API Rest da aplicação Voll.med, contendo as funcionalidades de CRUD de médicos e de pacientes, " +
+                "além de agendamento e cancelamento de consultas\"";
+        var contato = new Contact().email("Time Backend").email("backend@voll.med");
+        var licenca = new License().name("Apache 2.0").url("http://voll.med/api/licenca");
+        var informacoesApi = new Info().title(tituloApi).description(descricaoApi).contact(contato).license(licenca);
+
+        return new OpenAPI().components(components).info(informacoesApi);
     }
 }
